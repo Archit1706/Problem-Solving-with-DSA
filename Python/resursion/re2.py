f8c163a3742da4091df191381168169fadd2fb93
Archit1706 <architrathod77@gmail.com>
2024-09-23 06:52:13 -0800

    New Approach

--- Python/resursion/re2.py
+++ Python/resursion/re2.py
@@ -2,14 +2,33 @@
 
 n = 10
 
-def print_n(i):
-    if i > n:
-        return 
-    
-    print(i)
-    print_n(i + 1)
+# def print_n(i):
+#     if i > n:
+#         return
+
+#     print(i)
+#     print_n(i + 1)
+
+# def main():
+#     print_n(1)
+
+# main()
+
+
+def print_n(n):
+    # Base Condition
+    if n == 0:
+        return
+
+    # Hypothesis
+    print_n(n - 1)
+
+    # Induction
+    print(n)
+
 
 def main():
-    print_n(1)
+    print_n(n)
+
 
 main()
--- Python/resursion/re3.py
+++ Python/resursion/re3.py
@@ -2,14 +2,33 @@
 
 n = 10
 
-def print_reverse_n(i):
-    if i < 1:
+# def print_reverse_n(i):
+#     if i < 1:
+#         return
+
+#     print(i)
+#     print_reverse_n(i - 1)
+
+# def main():
+#     print_reverse_n(n)
+
+# main()
+
+
+def print_reverse(n):
+    # Base Condition
+    if n == 0:
         return
-    
-    print(i)
-    print_reverse_n(i - 1)
+
+    # Induction
+    print(n)
+
+    # Hypothesis
+    print_reverse(n - 1)
+
 
 def main():
-    print_reverse_n(n)
+    print_reverse(n)
+
 
 main()
